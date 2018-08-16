6e2f19f4232eb742f086c184047efcda27a1219e
eugenebrusov <eugenebrusov@gmail.com>
2018-08-16 11:15:31 +0300

    Create Android project

---
+++ BrusovCodeTest/app/src/main/java/com/eugenebrusov/brusovcodetest/RepoListActivity.kt
@@ -0,0 +1,12 @@
+package com.eugenebrusov.brusovcodetest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RepoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repo_list)
+    }
+}
